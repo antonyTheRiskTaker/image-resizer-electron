ce067460d16a0d70f7fca9cf3f4f8b77dc1deeee
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-11-25 02:41:21 +0800

    added necessary code to make app work

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+const path = require('path');
 const { app, BrowserWindow } = require('electron');
+
+const isMac = process.platform === 'darwin';
 
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
@@ -7,5 +10,22 @@ function createMainWindow() {
     height: 600
   });
 
-  mainWindow.loadFile(); // TODO: continue from here
+  mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
+
+app.whenReady().then(() => {
+  createMainWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+});
+
+// (Lines below) these lines of code make sure your desktop app is cross-platform.
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});

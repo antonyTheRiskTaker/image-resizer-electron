39ac26662458e06388e5cdb4a5d18d9155cc499c
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-12-09 10:13:26 +0800

    tutorial finished
    as the title suggests

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -27,6 +27,37 @@ function loadImage(e) {
   outputPath.innerText = path.join(os.homedir(), 'imageresized');
 }
 
+// Send image data to main (backend entry point file)
+function sendImage(e) {
+  e.preventDefault();
+
+  const width = widthInput.value;
+  const height = heightInput.value;
+  const imgPath = img.files[0].path;
+
+  if (!img.files[0]) {
+    alertError('Please upload an image');
+    return;
+  }
+
+  if (width === '' || height === '') {
+    alertError('Please fill in a height and width');
+    return;
+  }
+
+  // Send to main using ipcRenderer
+  ipcRenderer.send('image:resize', {
+    imgPath,
+    width,
+    height
+  });
+}
+
+// Catch the image:done event
+ipcRenderer.on('image:done', () => {
+  alertSuccess(`Image resized to ${widthInput.value} x ${heightInput.value}`);
+});
+
 // Make sure file is image
 function isFileImage(file) {
   const acceptedImageTypes = ['image/gif', 'image/png', 'image/jpeg'];
@@ -61,3 +92,4 @@ function alertSuccess(message) {
 
 // Eventlisteners
 img.addEventListener('change', loadImage);
+form.addEventListener('submit', sendImage);
